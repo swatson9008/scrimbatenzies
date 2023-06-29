a621d6c5c9db09f811b94488a0efd4061a6676d3
Summer <swatson9008@yahoo.com>
2023-06-28 20:59:37 -0400

    coded up the basic boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Die from './Die'
+
+export default function App() {
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      newDice.push(randomNumber);
+    }
+    return newDice;
+  }
+  
+  const newDie = allNewDice();
+  console.log(newDie);
+  return (
+      <main>
+          <div className="dice-container">
+              <Die value="1" />
+              <Die value="2" />
+              <Die value="3" />
+              <Die value="4" />
+              <Die value="5" />
+              <Die value="6" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+              <Die value="1" />
+          </div>
+      </main>
+  )
+}
