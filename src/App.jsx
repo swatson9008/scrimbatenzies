554c6cc906c9ac08bf36a60f17ee0b44066d92a6
Summer <swatson9008@yahoo.com>
2023-07-04 13:56:24 -0400

    winning condition works now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react"
 import Die from "./Die"
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
 
 
 export default function App() {
 
     const [dice, setDice] = React.useState(allNewDice())
+    const [turnCount, setTurnCount] = React.useState(0);
+    const [tenzies, setTenzies] = React.useState(false);
+
+    React.useEffect(() => {
+
+      const allDiceHeld = dice.every(die => die.isHeld === true);
+      const sameValue = dice.every(die => die.value === dice[0].value);
+      if (allDiceHeld && sameValue === true){setTenzies(true)}
+    
+    }, [dice]);
     
     function allNewDice() {
         const newDice = []
@@ -20,7 +31,25 @@ export default function App() {
     }
     
     function rollDice() {
-        setDice(allNewDice())
+      if (tenzies) {
+        setDice(allNewDice());
+        setTenzies(false);
+        setTurnCount(0)
+      }
+      else {
+        setDice(prevDice => {
+          return prevDice.map(die => {
+            if (die.isHeld === true) {
+              return {
+                ...die,
+              }
+            }
+            else (die.value = Math.ceil(Math.random() * 6))
+            return die
+          })
+        })
+        setTurnCount(prevTurnCount => prevTurnCount + 1);
+      }
     }
     
     console.log(dice)
@@ -48,10 +77,14 @@ export default function App() {
     
     return (
         <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls. See how low your turn count can go before you win!</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            <button className="roll-dice" onClick={rollDice}>{tenzies ? 'New Game' : 'Roll Dice'}</button>
+            <div>Turn Count: {turnCount}</div>
         </main>
     )
 }
